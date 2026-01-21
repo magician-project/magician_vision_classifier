ec02e9379b85161f3994a52d520ae0947c0e2ef4
Ammar Qammaz <ammarkov@gmail.com>
2026-01-21 14:49:35 +0200

    add a script to execute ROS node after setting up the environment

---
+++ scripts/runROSMagicianVisionClassifier.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#This script should be put in the root directory of the ROS workspace
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+#Let's load a sane virtual environment
+source ~/.bashrc
+source src/magician_vision_classifier/venv/bin/activate
+source install/setup.bash 
+
+cd src/magician_vision_classifier
+python3 liveClassifierTorchROS.py 
+
+
+exit 0
